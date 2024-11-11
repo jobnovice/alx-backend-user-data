3746eb6fdbe0908515a2f0bcf4fae89e59237e4b
jobnovice <jobkebede47@gmail.com>
2024-11-11 22:26:01 +0300

    pyvalidated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,16 +3,17 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """new class implemented for authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth"""
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """that returns None"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """current user"""
         return None
