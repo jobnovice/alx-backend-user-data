c6729615550ed8417e2d48297da77ca073a1d107
jobnovice <jobkebede47@gmail.com>
2024-11-13 01:04:38 +0300

    horrid

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,16 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Basic - User credentials"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        bf_co, sp, af_co = decoded_base64_authorization_header.partition(":")
+        return bf_co, af_co
