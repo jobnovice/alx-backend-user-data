1d5258e86c65cd6be612cb21e3fcc36bc36abbb2
jobnovice <jobkebede47@gmail.com>
2024-11-15 01:21:17 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 """new authentication mechanism"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session based authentication implemented"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a session id for the user with the user_id"""
+        if not user_id:
+            return None
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        ssid = str(uuid.uuid4())
+        self.user_id_by_session_id[f'{ssid}'] = user_id
+        return ssid
