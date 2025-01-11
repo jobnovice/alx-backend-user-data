c53094923a30cd785d5a49452dd6f40053a5365f
jobnovice <jobkebede47@gmail.com>
2025-01-12 01:49:52 +0300

    Register user using Abstraction mechanism

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 """seting minimal flask app"""
 from flask import Flask
+from flask import request
 from flask import jsonify
+from auth import Auth
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
 def hello():
     """simple binevenue message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """register user based on the credential sent"""
+    try:
+        user = AUTH.register_user(request.form['email'],
+                                  request.form['password'])
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": f"{request.form['email']}",
+                    "message": "user created"})
 
 
 if __name__ == "__main__":
