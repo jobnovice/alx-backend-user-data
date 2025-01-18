2144c4980b8c86c6c0322957c390a950a2f40cd4
jobnovice <jobkebede47@gmail.com>
2025-01-18 03:14:40 +0300

    slight reinterpretation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,8 @@ class Auth:
         user = self._db.find_user_by(reset_token=reset_token)
         if not user:
             raise ValueError()
+        new_pass = _hash_password(password)
         self._db.update_user(user.id,
-                             hashed_password=_hash_password(password),
+                             hashed_password=new_pass,
                              reset_token=None)
         return
